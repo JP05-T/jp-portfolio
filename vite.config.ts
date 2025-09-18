4647ccfc73e55d657ba5d902740f9ad1993d66c9
JP05-T <tjayaprakash60@gmail.com>
2025-09-18 22:01:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // 👈 important for custom domains (Netlify)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
